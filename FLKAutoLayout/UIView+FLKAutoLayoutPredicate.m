58ac7ff54ae859d7f75c9c892a816ec9133dfa37
Robert Gilliam <rhgills@gmail.com>
2013-07-01 18:46:31 -0700

    Fix misleading assert message when unable to find a common superview for a view

--- FLKAutoLayout/UIView+FLKAutoLayoutPredicate.m
+++ FLKAutoLayout/UIView+FLKAutoLayoutPredicate.m
@@ -42,7 +42,7 @@
         return self.superview;
     } else {
         UIView* commonSuperview = [self traverseViewTreeForCommonSuperViewWithView:view];
-        NSAssert(commonSuperview, @"Cannot find common superview of %@ and %@. Finding common superview in view tree not implemented yet", self, view);
+        NSAssert(commonSuperview, @"Cannot find common superview of %@ and %@. Did you forget to call addSubview: before adding constraints?", self, view);
         return commonSuperview;
     }
 }
