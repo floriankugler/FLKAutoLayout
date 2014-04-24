4e8868428275e99b8b7db9d9a63de2a2715632d6
Michael Rakowski <michael+git@mrakowski.com>
2014-04-23 21:10:20 -0400

    Add documentation comments for the first group of methods

--- FLKAutoLayout/UIView+FLKAutoLayout.h
+++ FLKAutoLayout/UIView+FLKAutoLayout.h
@@ -9,16 +9,77 @@
 
 @interface UIView (FLKAutoLayout)
 
+///---------------------------------------
+/// @name Aligning layout attributes
+///---------------------------------------
+
+/**
+  Align a layout attribute of a view to a layout attribute of a different view
+  @param attribute   The attribute to align
+  @param toAttribute The attribute to align to
+  @param view        The view of the attribute to align to
+  @param predicate   A predicate to use for alignment
+  @return An array of NSLayoutConstraint objects to the view
+ */
 - (NSArray*)alignAttribute:(NSLayoutAttribute)attribute toAttribute:(NSLayoutAttribute)toAttribute ofView:(UIView*)view predicate:(NSString*)predicate;
 
+///---------------------------------------
+/// @name Aligning to a view
+///---------------------------------------
 
+/**
+  Align a view to another view
+  @param view The view to align the view to
+  @return An array of NSLayoutConstraint objects applied to the view
+ */
 - (NSArray*)alignToView:(UIView*)view;
-- (NSArray*)alignTop:(NSString*)top leading:(NSString*)leading bottom:(NSString*)bottom trailing:(NSString*)trailing toView:(UIView*)view;
-- (NSArray*)alignTop:(NSString*)top leading:(NSString*)leading toView:(UIView*)view;
-- (NSArray*)alignBottom:(NSString*)bottom trailing:(NSString*)trailing toView:(UIView*)view;
-- (NSArray*)alignTop:(NSString*)top bottom:(NSString*)bottom toView:(UIView*)view;
-- (NSArray*)alignLeading:(NSString*)leading trailing:(NSString*)trailing toView:(UIView*)view;
 
+/**
+  Align a view to another view
+  @param top      The offset for the top alignment
+  @param leading  The offset for the leading alignment
+  @param bottom   The offset for the bottom alignment
+  @param trailing The offset for the trailing alignment
+  @param view     The view to align to
+  @return An array of NSLayoutConstraint objects applied to the view
+ */
+- (NSArray*)alignTop:(NSString*)top leading:(NSString*)leading bottom:(NSString*)bottom trailing:(NSString*)trailing toView:(UIView*)view;
+
+/**
+  Align a view to another view
+  @param top     The offset for the top alignment
+  @param leading The offset for the leading alignment
+  @param view    The view to align to
+  @return An array of NSLayoutConstraint objects applied to the view
+ */
+- (NSArray*)alignTop:(NSString*)top leading:(NSString*)leading toView:(UIView*)view;
+
+/**
+  Align a view
+  @param bottom   The offset for the bottom alignment
+  @param trailing The offset for the trailing alignment
+  @param view     The view to align to
+  @return An array of NSLayoutConstraint objects applied to the view
+ */
+- (NSArray*)alignBottom:(NSString*)bottom trailing:(NSString*)trailing toView:(UIView*)view;
+
+/**
+  Align a view
+  @param top    The offset for the top alignment
+  @param bottom The offset for the bottom alignment
+  @param view   The view to align to
+  @return An array of NSLayoutConstraint objects applied to the view
+ */
+- (NSArray*)alignTop:(NSString*)top bottom:(NSString*)bottom toView:(UIView*)view;
+
+/**
+  Align a view to another view
+  @param leading  The offset for the leading alignment
+  @param trailing The offset for the trailing alignment
+  @param view     The view to align to
+  @return An array of NSLayoutConstraint objects applied to the view
+ */
+- (NSArray*)alignLeading:(NSString*)leading trailing:(NSString*)trailing toView:(UIView*)view;
 
 - (NSArray*)alignLeadingEdgeWithView:(UIView*)view predicate:(NSString*)predicate;
 - (NSArray*)alignTrailingEdgeWithView:(UIView*)view predicate:(NSString*)predicate;
